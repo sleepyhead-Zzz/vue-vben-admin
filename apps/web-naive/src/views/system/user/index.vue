28638662a4a19d0af6ffe593e07d840655ef8e1f
Sleepyhead <realcowhorse@outlook.com>
2024-12-30 13:29:58 +0000

    ✨ 完善用户表格

--- apps/web-naive/src/views/system/user/index.vue
+++ apps/web-naive/src/views/system/user/index.vue
@@ -4,7 +4,7 @@ import type { VxeTableGridOptions } from '#/adapter/vxe-table';
 
 import { Page } from '@vben/common-ui';
 
-import { useMessage } from 'naive-ui';
+import { NButton, useMessage } from 'naive-ui';
 
 import { useVbenVxeGrid } from '#/adapter/vxe-table';
 import { ApiService, type UserDTO } from '#/apis';
@@ -22,9 +22,29 @@ const gridOptions: VxeTableGridOptions<UserDTO> = {
     highlight: true,
     labelField: 'name',
   },
+
   columns: [
     { field: 'userId', title: '用户ID' },
-    { field: 'roleName', title: '角色' },
+    { field: 'postId', title: '职位ID' },
+    { field: 'postName', title: '职位名称' },
+    { field: 'roleId', title: '角色ID' },
+    { field: 'roleName', title: '角色名称' },
+    { field: 'deptId', title: '部门ID' },
+    { field: 'deptName', title: '部门名称' },
+    { field: 'username', title: '用户名' },
+    { field: 'nickname', title: '昵称' },
+    { field: 'userType', title: '用户类型' },
+    { field: 'email', title: '邮箱' },
+    { field: 'photoNumber', title: '电话号码' },
+    { field: 'sex', title: '性别' },
+    { field: 'avatar', title: '头像' },
+    {
+      field: 'action',
+      fixed: 'right',
+      slots: { default: 'action' },
+      title: '操作',
+      width: 120,
+    },
   ],
   exportConfig: {},
   height: 'auto',
@@ -38,7 +58,13 @@ const gridOptions: VxeTableGridOptions<UserDTO> = {
       },
     },
   },
+
   toolbarConfig: {
+    tools: [
+      { name: '新增', code: 'add', status: 'primary' },
+      { name: '删除', code: 'del', status: 'error' },
+    ],
+
     custom: true,
     export: true,
     refresh: true,
@@ -47,15 +73,30 @@ const gridOptions: VxeTableGridOptions<UserDTO> = {
     zoom: true,
   },
 };
-
+function addUser() {}
+function deleteUsers() {}
+const gridEvents: VxeGridListeners = {
+  toolbarToolClick(params) {
+    if (params === del) {
+      addUser();
+    } else if (params === add) {
+      deleteUsers();
+    }
+  },
+};
 const [Grid] = useVbenVxeGrid({
   formOptions,
   gridOptions,
+  gridEvents,
 });
 </script>
 
 <template>
   <Page auto-content-height>
-    <Grid />
+    <Grid v-on="gridEvents">
+      <template #action>
+        <NButton type="primary">编辑</NButton>
+      </template>
+    </Grid>
   </Page>
 </template>
