fa1b5378829d489cd893968d12a5174feaa748fe
Sleepyhead <realcowhorse@outlook.com>
2025-08-01 12:06:27 +0000

    💄 移除未实现的登录方式

--- packages/effects/common-ui/src/ui/authentication/login.vue
+++ packages/effects/common-ui/src/ui/authentication/login.vue
@@ -14,7 +14,6 @@ import { useVbenForm } from '@vben-core/form-ui';
 import { VbenButton, VbenCheckbox } from '@vben-core/shadcn-ui';
 
 import Title from './auth-title.vue';
-import ThirdPartyLogin from './third-party-login.vue';
 
 interface Props extends AuthenticationProps {
   formSchema?: VbenFormSchema[];
@@ -148,14 +147,14 @@ defineExpose({
       v-if="showCodeLogin || showQrcodeLogin"
       class="mb-2 mt-4 flex items-center justify-between"
     >
-      <VbenButton
+      <!-- <VbenButton
         v-if="showCodeLogin"
         class="w-1/2"
         variant="outline"
         @click="handleGo(codeLoginPath)"
       >
         {{ $t('authentication.mobileLogin') }}
-      </VbenButton>
+      </VbenButton> -->
       <VbenButton
         v-if="showQrcodeLogin"
         class="ml-4 w-1/2"
@@ -167,11 +166,11 @@ defineExpose({
     </div>
 
     <!-- 第三方登录 -->
-    <slot name="third-party-login">
+    <!-- <slot name="third-party-login">
       <ThirdPartyLogin v-if="showThirdPartyLogin" />
-    </slot>
+    </slot> -->
 
-    <slot name="to-register">
+    <!-- <slot name="to-register">
       <div v-if="showRegister" class="mt-3 text-center text-sm">
         {{ $t('authentication.accountTip') }}
         <span
@@ -181,6 +180,6 @@ defineExpose({
           {{ $t('authentication.createAccount') }}
         </span>
       </div>
-    </slot>
+    </slot> -->
   </div>
 </template>
