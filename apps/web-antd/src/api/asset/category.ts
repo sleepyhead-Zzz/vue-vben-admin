5ea94300616d727932c83a0cb01d3eb3cd63427c
Sleepyhead <realcowhorse@outlook.com>
2025-12-24 06:40:05 +0000

    ✨ 更新接口客户端

--- apps/web-antd/src/api/asset/category.ts
+++ apps/web-antd/src/api/asset/category.ts
@@ -1,16 +1,16 @@
 // @ts-ignore
 /* eslint-disable */
-import request from "#/api/request";
+import request from '#/api/request';
 
 /** 新增资产分类 POST /asset/category */
 export async function addCategory(
   body: AssetAPI.AddAssetCategoryCommand,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
-  return request<AssetAPI.ResponseDTOVoid>("/asset/category", {
-    method: "POST",
+  return request<AssetAPI.ResponseDTOVoid>('/asset/category', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     data: body,
     ...(options || {}),
@@ -21,16 +21,16 @@ export async function addCategory(
 export async function getCategoryInfo(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: AssetAPI.getCategoryInfoParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   const { categoryId: param0, ...queryParams } = params;
   return request<AssetAPI.ResponseDTOAssetCategoryDTO>(
     `/asset/category/${param0}`,
     {
-      method: "GET",
+      method: 'GET',
       params: { ...queryParams },
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -39,13 +39,13 @@ export async function editCategory(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: AssetAPI.editCategoryParams,
   body: AssetAPI.UpdateAssetCategoryCommand,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   const { categoryId: param0, ...queryParams } = params;
   return request<AssetAPI.ResponseDTOVoid>(`/asset/category/${param0}`, {
-    method: "PUT",
+    method: 'PUT',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     params: { ...queryParams },
     data: body,
@@ -57,10 +57,10 @@ export async function editCategory(
 export async function batchRemoveCategory(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: AssetAPI.batchRemoveCategoryParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
-  return request<AssetAPI.ResponseDTOVoid>("/asset/category/batch-delete", {
-    method: "DELETE",
+  return request<AssetAPI.ResponseDTOVoid>('/asset/category/batch-delete', {
+    method: 'DELETE',
     params: {
       ...params,
     },
@@ -71,11 +71,11 @@ export async function batchRemoveCategory(
 /** 资产分类下拉列表 GET /asset/category/dropdown */
 export async function dropDownListCategory(options?: { [key: string]: any }) {
   return request<AssetAPI.ResponseDTOListAssetCategoryVO>(
-    "/asset/category/dropdown",
+    '/asset/category/dropdown',
     {
-      method: "GET",
+      method: 'GET',
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -83,14 +83,14 @@ export async function dropDownListCategory(options?: { [key: string]: any }) {
 export async function exportCategoryByExcel(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: AssetAPI.exportCategoryByExcelParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
-  return request<any>("/asset/category/excel", {
-    method: "GET",
+  return request<any>('/asset/category/excel', {
+    method: 'GET',
     params: {
       ...params,
       query: undefined,
-      ...params["query"],
+      ...params['query'],
     },
     ...(options || {}),
   });
@@ -98,8 +98,8 @@ export async function exportCategoryByExcel(
 
 /** 资产分类导入excel下载 GET /asset/category/excelTemplate */
 export async function downloadExcelTemplate6(options?: { [key: string]: any }) {
-  return request<any>("/asset/category/excelTemplate", {
-    method: "GET",
+  return request<any>('/asset/category/excelTemplate', {
+    method: 'GET',
     ...(options || {}),
   });
 }
@@ -110,25 +110,25 @@ export async function importCategoryByExcel(
     updateSupport?: boolean;
   },
   file?: File,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   const formData = new FormData();
 
   if (file) {
-    formData.append("file", file);
+    formData.append('file', file);
   }
 
   Object.keys(body).forEach((ele) => {
     const item = (body as any)[ele];
 
     if (item !== undefined && item !== null) {
-      if (typeof item === "object" && !(item instanceof File)) {
+      if (typeof item === 'object' && !(item instanceof File)) {
         if (item instanceof Array) {
-          item.forEach((f) => formData.append(ele, f || ""));
+          item.forEach((f) => formData.append(ele, f || ''));
         } else {
           formData.append(
             ele,
-            new Blob([JSON.stringify(item)], { type: "application/json" })
+            new Blob([JSON.stringify(item)], { type: 'application/json' }),
           );
         }
       } else {
@@ -137,10 +137,10 @@ export async function importCategoryByExcel(
     }
   });
 
-  return request<AssetAPI.ResponseDTOString>("/asset/category/importData", {
-    method: "POST",
+  return request<AssetAPI.ResponseDTOString>('/asset/category/importData', {
+    method: 'POST',
     data: formData,
-    requestType: "form",
+    requestType: 'form',
     ...(options || {}),
   });
 }
@@ -149,17 +149,17 @@ export async function importCategoryByExcel(
 export async function listCategory(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: AssetAPI.listCategoryParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   return request<AssetAPI.ResponseDTOListAssetCategoryDTO>(
-    "/asset/category/list",
+    '/asset/category/list',
     {
-      method: "GET",
+      method: 'GET',
       params: {
         ...params,
       },
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -167,17 +167,17 @@ export async function listCategory(
 export async function getPagedCategorys(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: AssetAPI.getPagedCategorysParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   return request<AssetAPI.ResponseDTOPageDTOAssetCategoryDTO>(
-    "/asset/category/page",
+    '/asset/category/page',
     {
-      method: "GET",
+      method: 'GET',
       params: {
         ...params,
       },
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -185,11 +185,11 @@ export async function getPagedCategorys(
 export async function removeCategory(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: AssetAPI.removeCategoryParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   const { categoryId: param0, ...queryParams } = params;
   return request<AssetAPI.ResponseDTOVoid>(`/asset/category/remove/${param0}`, {
-    method: "DELETE",
+    method: 'DELETE',
     params: { ...queryParams },
     ...(options || {}),
   });
