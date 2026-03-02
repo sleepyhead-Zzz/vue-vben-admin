1ec5c977c0fcac43664b2418897b843b769a50c6
Sleepyhead <realcowhorse@outlook.com>
2026-03-02 08:21:07 +0000

    🎨 格式化代码

--- apps/web-antd/src/api/perf/salesdata.ts
+++ apps/web-antd/src/api/perf/salesdata.ts
@@ -1,16 +1,16 @@
 // @ts-ignore
 /* eslint-disable */
-import request from "#/api/request";
+import request from '#/api/request';
 
 /** 新增销售明细事实 POST /perf/salesdata */
 export async function addSalesData(
   body: PerfAPI.AddPerfFactSalesDataCommand,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
-  return request<PerfAPI.ResponseDTOVoid>("/perf/salesdata", {
-    method: "POST",
+  return request<PerfAPI.ResponseDTOVoid>('/perf/salesdata', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     data: body,
     ...(options || {}),
@@ -21,10 +21,10 @@ export async function addSalesData(
 export async function removeSalesData(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: PerfAPI.removeSalesDataParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
-  return request<PerfAPI.ResponseDTOVoid>("/perf/salesdata", {
-    method: "DELETE",
+  return request<PerfAPI.ResponseDTOVoid>('/perf/salesdata', {
+    method: 'DELETE',
     params: {
       ...params,
     },
@@ -37,13 +37,13 @@ export async function editSalesData(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: PerfAPI.editSalesDataParams,
   body: PerfAPI.UpdatePerfFactSalesDataCommand,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   const { saleId: param0, ...queryParams } = params;
   return request<PerfAPI.ResponseDTOVoid>(`/perf/salesdata/${param0}`, {
-    method: "PUT",
+    method: 'PUT',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     params: { ...queryParams },
     data: body,
@@ -55,16 +55,16 @@ export async function editSalesData(
 export async function getSalesDataInfo(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: PerfAPI.getSalesDataInfoParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   const { salesId: param0, ...queryParams } = params;
   return request<PerfAPI.ResponseDTOPerfFactSalesDataDTO>(
     `/perf/salesdata/${param0}`,
     {
-      method: "GET",
+      method: 'GET',
       params: { ...queryParams },
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -72,14 +72,14 @@ export async function getSalesDataInfo(
 export async function exportSalesDataByExcel(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: PerfAPI.exportSalesDataByExcelParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
-  return request<any>("/perf/salesdata/excel", {
-    method: "GET",
+  return request<any>('/perf/salesdata/excel', {
+    method: 'GET',
     params: {
       ...params,
       query: undefined,
-      ...params["query"],
+      ...params['query'],
     },
     ...(options || {}),
   });
@@ -91,25 +91,25 @@ export async function importSalesDataByExcel(
     request?: PerfAPI.SalesDataImportRequest;
   },
   file?: File,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   const formData = new FormData();
 
   if (file) {
-    formData.append("file", file);
+    formData.append('file', file);
   }
 
   Object.keys(body).forEach((ele) => {
     const item = (body as any)[ele];
 
     if (item !== undefined && item !== null) {
-      if (typeof item === "object" && !(item instanceof File)) {
+      if (typeof item === 'object' && !(item instanceof File)) {
         if (item instanceof Array) {
-          item.forEach((f) => formData.append(ele, f || ""));
+          item.forEach((f) => formData.append(ele, f || ''));
         } else {
           formData.append(
             ele,
-            new Blob([JSON.stringify(item)], { type: "application/json" })
+            new Blob([JSON.stringify(item)], { type: 'application/json' }),
           );
         }
       } else {
@@ -119,13 +119,13 @@ export async function importSalesDataByExcel(
   });
 
   return request<PerfAPI.ResponseDTOImportResponseDTO>(
-    "/perf/salesdata/importData",
+    '/perf/salesdata/importData',
     {
-      method: "POST",
+      method: 'POST',
       data: formData,
-      requestType: "form",
+      requestType: 'form',
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -133,28 +133,28 @@ export async function importSalesDataByExcel(
 export async function getSalesDataList(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: PerfAPI.getSalesDataListParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   return request<PerfAPI.ResponseDTOListPerfFactSalesDataDTO>(
-    "/perf/salesdata/list",
+    '/perf/salesdata/list',
     {
-      method: "GET",
+      method: 'GET',
       params: {
         ...params,
       },
       ...(options || {}),
-    }
+    },
   );
 }
 
 /** 销售明细事实下拉列表 GET /perf/salesdata/option-select */
 export async function optionSalesDataSelect(options?: { [key: string]: any }) {
   return request<PerfAPI.ResponseDTOListPerfFactSalesDataVO>(
-    "/perf/salesdata/option-select",
+    '/perf/salesdata/option-select',
     {
-      method: "GET",
+      method: 'GET',
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -162,17 +162,17 @@ export async function optionSalesDataSelect(options?: { [key: string]: any }) {
 export async function getPagedSalesData(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: PerfAPI.getPagedSalesDataParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   return request<PerfAPI.ResponseDTOPageDTOPerfFactSalesDataDTO>(
-    "/perf/salesdata/page",
+    '/perf/salesdata/page',
     {
-      method: "GET",
+      method: 'GET',
       params: {
         ...params,
       },
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -180,11 +180,11 @@ export async function getPagedSalesData(
 export async function removeSalesDataById(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: PerfAPI.removeSalesDataByIdParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   const { salesId: param0, ...queryParams } = params;
   return request<PerfAPI.ResponseDTOVoid>(`/perf/salesdata/remove/${param0}`, {
-    method: "DELETE",
+    method: 'DELETE',
     params: { ...queryParams },
     ...(options || {}),
   });
