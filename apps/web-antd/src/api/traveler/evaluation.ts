d1462a943adf7c8a5a8e91e174d6a1a76ba3f666
Sleepyhead <realcowhorse@outlook.com>
2026-03-02 18:00:14 +0000

    ✨ 格式化代码

--- apps/web-antd/src/api/traveler/evaluation.ts
+++ apps/web-antd/src/api/traveler/evaluation.ts
@@ -1,16 +1,16 @@
 // @ts-ignore
 /* eslint-disable */
-import request from "#/api/request";
+import request from '#/api/request';
 
 /** 新增评价 POST /traveler/evaluation */
 export async function addEvaluation(
   body: TravelerAPI.AddEvaluationCommand,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
-  return request<TravelerAPI.ResponseDTOVoid>("/traveler/evaluation", {
-    method: "POST",
+  return request<TravelerAPI.ResponseDTOVoid>('/traveler/evaluation', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     data: body,
     ...(options || {}),
@@ -21,16 +21,16 @@ export async function addEvaluation(
 export async function getEvaluationInfo(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: TravelerAPI.getEvaluationInfoParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   const { evaluationId: param0, ...queryParams } = params;
   return request<TravelerAPI.ResponseDTOTravelerEvaluationDTO>(
     `/traveler/evaluation/${param0}`,
     {
-      method: "GET",
+      method: 'GET',
       params: { ...queryParams },
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -39,20 +39,20 @@ export async function editEvaluation(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: TravelerAPI.editEvaluationParams,
   body: TravelerAPI.UpdateEvaluationCommand,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   const { evaluationId: param0, ...queryParams } = params;
   return request<TravelerAPI.ResponseDTOVoid>(
     `/traveler/evaluation/${param0}`,
     {
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       params: { ...queryParams },
       data: body,
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -60,16 +60,16 @@ export async function editEvaluation(
 export async function getEvaluationInfoByBookId(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: TravelerAPI.getEvaluationInfoByBookIdParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   const { bookId: param0, ...queryParams } = params;
   return request<TravelerAPI.ResponseDTOTravelerEvaluationDTO>(
     `/traveler/evaluation/${param0}/book`,
     {
-      method: "GET",
+      method: 'GET',
       params: { ...queryParams },
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -77,17 +77,17 @@ export async function getEvaluationInfoByBookId(
 export async function removeEvaluation(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: TravelerAPI.removeEvaluationParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   return request<TravelerAPI.ResponseDTOVoid>(
-    "/traveler/evaluation/batch-delete",
+    '/traveler/evaluation/batch-delete',
     {
-      method: "DELETE",
+      method: 'DELETE',
       params: {
         ...params,
       },
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -95,17 +95,17 @@ export async function removeEvaluation(
 export async function getPagedEvaluation(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: TravelerAPI.getPagedEvaluationParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   return request<TravelerAPI.ResponseDTOPageDTOTravelerEvaluationDTO>(
-    "/traveler/evaluation/page",
+    '/traveler/evaluation/page',
     {
-      method: "GET",
+      method: 'GET',
       params: {
         ...params,
       },
       ...(options || {}),
-    }
+    },
   );
 }
 
@@ -113,15 +113,15 @@ export async function getPagedEvaluation(
 export async function removeEvaluationById(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: TravelerAPI.removeEvaluationByIdParams,
-  options?: { [key: string]: any }
+  options?: { [key: string]: any },
 ) {
   const { evaluationId: param0, ...queryParams } = params;
   return request<TravelerAPI.ResponseDTOVoid>(
     `/traveler/evaluation/remove/${param0}`,
     {
-      method: "DELETE",
+      method: 'DELETE',
       params: { ...queryParams },
       ...(options || {}),
-    }
+    },
   );
 }
